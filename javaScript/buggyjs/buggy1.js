c8855fc2831c3985016b734e8e520231607ae9d2
Assefa <assefamit@gamil.com>
2019-12-08 21:05:58 -0600

    javaScript

---
+++ javaScript/buggyjs/buggy1.js
@@ -0,0 +1,7 @@
+function gogogo() {
+    Alert("Yes, it works!");
+}
+window.onerror = function(message){
+        alert(message);
+        console.log(message);
+    };
---
+++ javaScript/buggyjs/correctedBuggy3.js
@@ -0,0 +1,7 @@
+function gogogo() {
+    alert("Yay, it works!");
+}
+window.onerror = function(message){
+        alert(message);
+        console.log(message);
+    };
